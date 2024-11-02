27501201ac705f0bc79c5a7022fe82f3ec836b29
houghdaniel <hough.daniel.s@gmail.com>
2024-11-02 00:24:42 -0500

    Added gaps in trees for farm and house (still have error)

--- Assets/TreeSpawner.cs
+++ Assets/TreeSpawner.cs
@@ -19,8 +19,21 @@ public class TreeSpawner : MonoBehaviour
             GameObject treePrefab = treePrefabs[Random.Range(0, treePrefabs.Length-1)];
             
             // Generate random position within the terrain bounds
-            float x = Random.Range(0, terrain.terrainData.size.x-1);
-            float z = Random.Range(0, terrain.terrainData.size.z-1);
+            float x = Random.Range(0, terrain.terrainData.size.x);
+            float z = Random.Range(0, terrain.terrainData.size.z);
+            //if(x>20 && x<80 && z>80 && z<130)
+            //{
+            //    i--;
+            //    continue;
+            //}
+
+            if((Mathf.Clamp(x,20,80)==x && Mathf.Clamp(z,80,130)==z) || (Mathf.Clamp(x,20,70)==x && Mathf.Clamp(z,20,40)==z))
+            {
+                i--;
+                continue;
+            }
+            
+
             float y = terrain.SampleHeight(new Vector3(x, 0, z)) + terrain.GetPosition().y;
 
             // Instantiate the tree prefab
