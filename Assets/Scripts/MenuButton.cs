8de31bcc1598e43d29c4477412dded564d7a7cce
houghdaniel <hough.daniel.s@gmail.com>
2025-11-08 10:18:45 -0600

    Created Level Select scenes and began linking them together

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuButton : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] private SceneAsset sceneAsset;
+#endif
+    [SerializeField, HideInInspector] private string sceneToLoad;
+
+    public void LoadScene()
+    {
+        if (!string.IsNullOrEmpty(sceneToLoad))
+            SceneManager.LoadScene(sceneToLoad);
+        else
+            Debug.LogWarning($"[MenuButton] No scene name assigned on {gameObject.name}");
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (sceneAsset != null)
+            sceneToLoad = sceneAsset.name;
+    }
+#endif
+}
